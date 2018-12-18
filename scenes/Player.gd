54048e7fcd7e275818941beee4a04a02bb2e92fd
Jacob Strong <ja.ha.strong@gmail.com>
2018-12-17 19:53:21 -0700

    first milestone

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,33 +1,30 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-export (int) var speed
+export (int) var run_speed = 350
+export (int) var jump_speed = -1000
+export (int) var gravity = 2500
 var screensize
+
+var vel = Vector2()
 
 func _ready():
 	screensize = get_viewport_rect().size
 	pass
 
-func _process(delta):
-	var velocity = Vector2() # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	#if Input.is_action_pressed("ui_up"):
-		#velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-	if Input.is_action_just_pressed("ui_up"):
-		velocity.y -= 100
+func get_input():
+	vel.x = 0
+	var right = Input.is_action_pressed('ui_right')
+	var left = Input.is_action_pressed('ui_left')
+	var jump = Input.is_action_pressed('ui_up')
 	
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screensize.x)
-	#position.y = clamp(position.y, 0, screensize.y)
-	pass
+	if is_on_floor() and jump:
+		vel.y = jump_speed
+	if right:
+		vel.x += run_speed
+	if left:
+		vel.x -= run_speed
 
-func start(pos):
-	position = pos
-	$CollisionShape2D.disabled = false
-	pass
+func _physics_process(delta):
+	vel.y += gravity * delta
+	get_input()
+	vel = move_and_slide(vel, Vector2(0, -1))
