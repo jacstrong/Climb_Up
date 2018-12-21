110bfbe568280933cb54934c5f64ca042a750cf2
Jacob Strong <ja.ha.strong@gmail.com>
2018-12-20 19:02:51 -0700

    plats dynamically come and go

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -1,14 +1,69 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var camera
+var screensize
+var platforms = []
+var Platforms
+
+var large_plat
+var small_plat
+
+var plats = []
+
+var last_level
+var levels = []
 
 func _ready():
+	randomize()
+	
+	# Initialize class vars
+	screensize = get_viewport_rect().size
+	camera = $Player/Camera2D.get_viewport_transform()
+	Platforms = $Platforms
+	last_level = camera.get_origin().y + 200
+	
+	# platforms
+	large_plat = preload("res://scenes/Floating_Floor.tscn")
+	plats.append(large_plat)
+	small_plat = preload("res://scenes/Floor.tscn")
+	plats.append(small_plat)
+	
+	# Make camera current
 	$Player/Camera2D.make_current()
+	
+	for x in [400, 200 , 0, -200]:
+		levels.push_front(x)
+		placePlats(x)
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	if ($Player/Camera2D.get_viewport_transform() != camera):
+		cameraMoved()
+		#print($Player/Camera2D.get_viewport_transform())
+	pass
+
+func cameraMoved():
+	camera = $Player/Camera2D.get_viewport_transform()
+	if (camera.get_origin().y) > (last_level + 200):
+		placePlats((last_level + 200) * -1)
+		levels.push_front((last_level + 200) * -1)
+		print(levels)
+		removePlats(levels.back())
+		levels.pop_back()
+		last_level = camera.get_origin().y
+
+func placePlats(y):
+	var num = floor(rand_range(2, 5))
+	for i in range(num):
+		var plat = plats[randi() % plats.size()].instance()
+		plat.position.y = y
+		plat.position.x = rand_range(0, screensize.x)
+		$Platforms.add_child(plat)
+		platforms.push_front(plat)
+		$Player/Camera2D/Control/NumPlats.text = String(platforms.size())
+
+func removePlats(y):
+	while platforms.back().position.y == y:
+		platforms.back().queue_free()
+		platforms.pop_back()
+	
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -3,9 +3,11 @@ extends KinematicBody2D
 export (int) var run_speed = 350
 export (int) var jump_speed = -1000
 export (int) var gravity = 2500
+export (int) var double_jump_speed = -500
 var screensize
 
 var vel = Vector2()
+var double_jump = true
 
 func _ready():
 	screensize = get_viewport_rect().size
@@ -15,10 +17,17 @@ func get_input():
 	vel.x = 0
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
-	var jump = Input.is_action_pressed('ui_up')
+	var jump = Input.is_action_just_pressed('ui_up')
 	
 	if is_on_floor() and jump:
 		vel.y = jump_speed
+		double_jump = true
+	elif !is_on_floor() and jump and double_jump:
+		if vel.y < 0:
+			vel.y += double_jump_speed
+		else:
+			vel.y = double_jump_speed
+		double_jump = false
 	if right:
 		vel.x += run_speed
 	if left:
